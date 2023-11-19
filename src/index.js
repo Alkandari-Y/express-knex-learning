13aaea2dff3cd6d7c5b8940ee4db1477a58b84cd
Alkandari-Y <56042101+Alkandari-Y@users.noreply.github.com>
2023-11-20 00:46:33 +0300

    index refactor

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const session = require("express-session");
 
-const { PORT } = require("./config").app;
-const sessionConfig = require("./config").session;
+// const { PORT } = require("./config").app;
+// const sessionConfig = require("./config").session;
+// Below is a one liner to achieve the above with destructuring and renaming and nested destructuring to avoid variable name collisions
+const { session: sessionConfig, app: { PORT } } = require('./config')
 const apiRoutes = require("./api/routes");
 const notFound = require("./middlewares/errorHandlers/notFound");
 const errorHandler = require("./middlewares/errorHandlers/errorHandler");
